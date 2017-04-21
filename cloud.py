b4e8dc54c9e0f656a6558f53cb20933fff61b3d5
sunny <87704470@qq.com>
2017-04-22 07:08:15 +0800

    添加云消息处理函数

--- cloud.py
+++ cloud.py
@@ -24,3 +24,31 @@ def before_todo_save(todo):
         raise LeanEngineError('内容不能为空')
     if len(content) >= 240:
         todo.set('content', content[:240] + ' ...')
+
+@engine.define
+def _messageReceived(**params):
+    # params = {
+    #     'fromPeer': 'Tom',
+    #     'receipt': false,
+    #     'groupId': null,
+    #     'system': null,
+    #     'content': '{"_lctext":"耗子，起床！","_lctype":-1}',
+    #     'convId': '5789a33a1b8694ad267d8040',
+    #     'toPeers': ['Jerry'],
+    #     '__sign': '1472200796787,a0e99be208c6bce92d516c10ff3f598de8f650b9',
+    #     'bin': false,
+    #     'transient': false,
+    #     'sourceIP': '121.239.62.103',
+    #     'timestamp': 1472200796764,
+    # }
+    print('_messageReceived start')
+    content = json.loads(params['content'])
+    text = content._lctext
+    print('text:', text)
+    processed_content = '12345'
+    print('_messageReceived end')
+    # 必须含有以下语句给服务端一个正确的返回，否则会引起异常
+    return {
+        'content': processed_content,
+    }
+
